1704a3e17c21b2fa9a80fd09b0ddfae37c420411
DABA <dabadiop2@yahoo.com>
2022-11-14 15:27:01 +0000

    implementation model et index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test   crud avec angular  et spring boot ';
+  title = 'Gestion des Employes ';
 }
